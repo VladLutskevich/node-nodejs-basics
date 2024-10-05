4451a2088fbcd9e9d8e0699b59c2428482e1aeac
Vlad Lutskevich <vlad01309@mail.ru>
2024-10-05 21:15:44 +0300

    feat: implement "fs list" task

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,16 @@
+import { join } from "node:path";
+import { readdir } from "node:fs/promises";
+
 const list = async () => {
-    // Write your code here 
+    const path = join('src', 'fs', 'files');
+
+    try {
+        const files = await readdir(path);
+        console.log(files);
+    }
+    catch(error) {
+        console.error(new Error('FS operation failed'));
+    }
 };
 
 await list();
