10b4404be2c46cca4efa53b6e3be78f8b5b1c0e3
Vlad Lutskevich <vlad01309@mail.ru>
2024-10-05 20:26:18 +0300

    feat: implement "fs copy" task

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,21 @@
+import { join } from "node:path";
+import { cp } from "node:fs/promises";
+
 const copy = async () => {
-    // Write your code here 
+    const path = join('src', 'fs', 'files');
+    const newPath = join('src', 'fs', 'files_copy');
+    const options = {
+        recursive: true,
+        errorOnExist: true,
+        force: false,
+    };
+
+    try {
+        await cp(path, newPath, options);
+    }
+    catch(error) {
+        console.error(new Error('FS operation failed'));
+    }
 };
 
 await copy();
