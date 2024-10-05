5a49dc4715b61a3bd229a6044a917978f8ef9e2e
Vlad Lutskevich <vlad01309@mail.ru>
2024-10-06 00:18:35 +0300

    feat: implement "hash" task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
+import { join } from "node:path";
+import { readFile } from "node:fs/promises";
+import { createHash } from "node:crypto";
+import { createReadStream } from "node:fs";
+
 const calculateHash = async () => {
-    // Write your code here 
+    const path = join('src', 'hash', 'files', 'fileToCalculateHashFor.txt');
+    let text = await readFile(path);
+    text = createHash("sha256");
+
+    const stream = createReadStream(path);
+    stream.on("data", (res) => text.update(res));
+    stream.on("end", () => console.log(text.digest("hex")));
 };
 
 await calculateHash();
