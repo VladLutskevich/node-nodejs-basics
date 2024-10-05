1406d724b4eab16266ca307c94096690bc72b428
Vlad Lutskevich <vlad01309@mail.ru>
2024-10-05 23:07:59 +0300

    feat: implement "cli args" task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,8 @@
 const parseArgs = () => {
-    // Write your code here 
+    const result = process.argv.slice(2).reduce((acc, el) => {
+        return el.startsWith('--') ? acc + ' ' + el.slice(2) + ' is ' : acc + el + ','; 
+      }, '');
+      console.log(result.slice(0, -1));
 };
 
 parseArgs();
