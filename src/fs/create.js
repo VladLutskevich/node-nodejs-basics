7e1e89b4d320a1d9fc59f95305c30ff576645580
Vlad Lutskevich <vlad01309@mail.ru>
2024-10-05 19:58:17 +0300

    feat: implement "fs create" task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import { join } from "node:path";
+import { writeFile } from "node:fs/promises";
+
 const create = async () => {
-    // Write your code here 
+    const path = join('src', 'fs', 'files', 'fresh.txt');
+    const text = 'I am fresh and young';
+    const options = {
+        flag: 'wx',
+        encoding: 'utf-8',
+    };
+
+    try {
+        await writeFile(path, text, options);
+    }
+    catch(error) {
+        console.error(new Error('FS operation failed'));
+    }
 };
 
 await create();
