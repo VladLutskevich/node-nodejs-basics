18cb6be41c4c26e9cf14336f6bfbbbeb6bf56823
vladlutskevich <vlad01309@mail.ru>
2024-10-07 13:01:09 +0300

    feat: implement "streams read" task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,12 @@
+import { join } from "node:path";
+import { createReadStream } from "node:fs";
+import process from "node:process";
+
 const read = async () => {
-    // Write your code here 
+    const path = join('src', 'streams', 'files', 'fileToRead.txt');
+
+    const stream = createReadStream(path);
+    stream.on("data", (res) => process.stdout.write(res));
 };
 
 await read();
